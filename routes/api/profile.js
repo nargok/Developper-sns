cbdae9b4036820d1ea84dbc45d17b6a0d5c1c540
Ryoichi Gokan <nargok@gmail.com>
2018-08-18 09:04:01 +0900

    delete profile ecucation & profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -257,4 +257,43 @@ router.delete(
     .catch((err) => res.status(404).json(err)); 
 });
 
+// @route  DELETE api/profile/education/:exp_id
+// @desc   Delete experience from profile
+// @access Private
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) =>{
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      // Get remove index
+      const removeIndex = profile.education
+        .map(item => item.id)
+        .indexOf(req.params.edu_id);
+
+      // Splice out of array
+      profile.education.splice(removeIndex, 1);
+
+      // Save
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch((err) => res.status(404).json(err));
+});
+
+// @route  DELETE api/profile/
+// @desc   Delete profile
+// @access Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) =>{
+    Profile.findOneAndRemove({ user: req.user.id})
+      .then(() => {
+        User.findOneAndRemove({ _id: req.user.id})
+          .then(() => {
+            res.json({ success: true })
+          })
+      })
+});
+
 module.exports = router;
