82c1d85aa00e6ca58e450bc0bbe7697c1863f572
Ryoichi Gokan <nargok@gmail.com>
2018-08-18 09:40:10 +0900

    post API with validation

--- models/Post.js
+++ models/Post.js
@@ -39,13 +39,13 @@ const PostSchema = new Schema({
       },
       date: {
         type: Date,
-        default: Date,now
+        default: Date.now
       }
     }
   ],
   date: {
     type: Date,
-    default: Date,now
+    default: Date.now
   }
 });
 
